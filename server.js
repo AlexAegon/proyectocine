4d8ba0d15b3fb4fbf0158234ad38233d42cac3c7
TransportesMundiales <138054879+TransportesMundiales@users.noreply.github.com>
2024-03-30 12:39:54 -0600

    Primer Commit

---
+++ server.js
@@ -0,0 +1,11 @@
+// crear App Express
+import  express  from "express";
+/**
+ * 1.- Importar Express
+ * 2.- Crear app con express 
+ * 3.- Usar app.listen para abrir puertos 
+ */
+const api= express(); // crea la Appi con express
+api.listen(8000, () =>{
+    
+}); // Abrir Los puertos en esta caso 8000 
